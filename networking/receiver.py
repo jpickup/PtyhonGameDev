db71ea50c3ec1b7b560448da66b16833125c7440
John Pickup <home@johnpickup.com>
2024-11-09 09:33:37 +0000

    try to get UDP working

--- networking/receiver.py
+++ networking/receiver.py
@@ -1,11 +1,26 @@
 import socket
 import pickle
+import struct
 from data import Data
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('', 12000))
+multicast_group = '224.1.1.1' 
+multicast_port = 12000
+
+receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+try:
+    receiver_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except AttributeError:
+    pass
+receiver_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+receiver_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+
+receiver_socket.bind((multicast_group, multicast_port))
+host = socket.gethostbyname(socket.gethostname())
+receiver_socket.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(host))
+receiver_socket.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP, socket.inet_aton(multicast_group) + socket.inet_aton(host))
 
 while True:
-    message, address = server_socket.recvfrom(1024)
+    message, address = receiver_socket.recvfrom(1024)
     data = pickle.loads(message)
+    print("Message from " + str(address))
     print(data.toString())
--- networking/sender.py
+++ networking/sender.py
@@ -2,13 +2,18 @@ import time
 import socket
 from data import Data
 import pickle
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(1.0)
-
+multicast_group = '224.1.1.1' 
+multicast_port = 12000
+MULTICAST_TTL = 32
+sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sender_socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+sender_socket.settimeout(1.0)
+addr = (multicast_group, multicast_port)
+    
 while True:
-    msg = input("Please enter the message:\n")
-    addr = ("127.0.0.1", 12000)
+    msg = "Hello world!"
     data = Data(msg)
 
     start = time.time()
-    client_socket.sendto(pickle.dumps(data), addr)
+    sender_socket.sendto(pickle.dumps(data), addr)
+    time.sleep(1)
