a99a1ba3c7a05bafc5edec1a67d9770c4994ad8c
John Pickup <home@johnpickup.com>
2024-11-13 19:55:49 +0000

    can we use one socket?

--- networking/networking.py
+++ networking/networking.py
@@ -6,12 +6,19 @@ class Network():
     def __init__(self, port = 55000, group = '255.255.255.255'):
         self.multicast_port = port
         self.multicast_group = group 
+        local_hostname = socket.gethostname()
+        ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+        filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+        self.local_ip = filtered_ips[:1][0]
+        print(self.local_ip)
         self.sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sender_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sender_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sender_socket.settimeout(1.0)
-        self.receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.receiver_socket.bind(('', self.multicast_port))
+        self.sender_socket.bind((self.local_ip, self.multicast_port))
+        #self.receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        #self.receiver_socket.bind(('', self.multicast_port))
+        self.receiver_socket = self.sender_socket
 
     def send(self, data):
         self.sender_socket.sendto(pickle.dumps(data), (self.multicast_group, self.multicast_port))
