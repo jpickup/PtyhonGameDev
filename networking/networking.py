238c7062247d1c0da8aa78ec4a785250e0b85438
John Pickup <home@johnpickup.com>
2024-11-13 20:20:36 +0000

    ?

--- networking/networking.py
+++ networking/networking.py
@@ -6,20 +6,18 @@ class Network():
     def __init__(self, port = 55000, group = '255.255.255.255'):
         self.multicast_port = port
         self.multicast_group = group 
-        local_hostname = socket.gethostname()
-        ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
-        filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
-        if (len(filtered_ips)>0):
-            self.local_ip = filtered_ips[:1][0]
-        else:
-            self.local_ip = None
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+        self.local_ip = s.getsockname()[0]
+        print(self.local_ip)
         self.sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sender_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sender_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sender_socket.settimeout(1.0)
-        #self.sender_socket.bind((self.local_ip, self.multicast_port))
-        self.receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.receiver_socket.bind(('', self.multicast_port))
+        self.sender_socket.bind((self.local_ip, self.multicast_port))
+        #self.receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        #self.receiver_socket.bind(('', self.multicast_port))
+        self.receiver_socket = self.sender_socket
         
     def send(self, data):
         self.sender_socket.sendto(pickle.dumps(data), (self.multicast_group, self.multicast_port))
